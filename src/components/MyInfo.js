e7a29f12b645530260cb836f0cfb84b6682203d0
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-29 11:38:11 +0530

    Separate file for components Created separate file for MyInfo component

---
+++ src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h4>Hi am Bala!</h4>
+      <p>Am learning React</p>
+      <ol>
+        <li>Bali</li>
+        <li>Andaman</li>
+        <li>Kerala</li>
+      </ol>
+    </div>
+  );
+}
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function MyInfo() {
-  return (
-    <g>
-      <h4>Hi am Bala!</h4>
-      <p>Am learning React</p>
-      <ol>
-        <li>Bali</li>
-        <li>Andaman</li>
-        <li>Kerala</li>
-      </ol>
-    </g>
-  );
-}
+import MyInfo from "./components/MyInfo";
 
 let rootElement = document.getElementById("root");
 ReactDOM.render(<MyInfo />, rootElement);
