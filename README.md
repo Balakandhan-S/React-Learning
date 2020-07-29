82e2533995224a656e8207ba5436faa0f58c0228
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-29 10:42:38 +0530

    1st React code Adding a unordered List as part of React Learning

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React-Learning
+
 Created with CodeSandbox
--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<div />, rootElement);
+ReactDOM.render(
+  <ul>
+    <li>Item1</li>
+    <li>Item2</li>
+    <li>Item3</li>
+  </ul>,
+  rootElement
+);
