02763399e23ad0e1da4b2e12e2ec49d300696d1a
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-29 11:05:43 +0530

    1st component made 1st functional component in React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function MyApp() {
+  return (
+    <ul>
+      <li>Item1</li>
+      <li>Item2</li>
+      <li>Item3</li>
+    </ul>
+  );
+}
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <ul>
-    <li>Item1</li>
-    <li>Item2</li>
-    <li>Item3</li>
-  </ul>,
-  rootElement
-);
+ReactDOM.render(<MyApp />, rootElement);
